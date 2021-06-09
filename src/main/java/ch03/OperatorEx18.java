e43acbd2a2951b6f77916341dbfb5aa03c67aeb9
HongEunbeen <37995685+HongEunbeen@users.noreply.github.com>
2021-06-10 00:07:47 +0900

    feat : 3장 마무리

--- src/main/java/ch03/OperatorEx18.java
+++ src/main/java/ch03/OperatorEx18.java
@@ -5,5 +5,7 @@ public class OperatorEx18 {
         double pi = 3.141592;
         double shortPi = Math.round(pi * 1000) / 1000.0;
         System.out.println("shortPi = " + shortPi);
+        //round 메서드는 매개변수로 받은 값을 소수점 첫째자리에서 반올림하고 그 결과를 정수로 돌려주는 메서드이다.
+
     }
 }
