110d3ed77b11da8d1208cd69f6b21e0da5bf8de2
HongEunbeen <vvvv980@gmail.com>
2021-06-18 00:03:37 +0900

    feat : 7장 상속 관련 예제 추가

---
+++ src/main/java/ch07/TVCR.java
@@ -0,0 +1,19 @@
+package ch07;
+
+class VCR{
+    boolean power;
+    int counter = 0;
+    void power(){power = !power;}
+    void play(){/* 내용 생략 */}
+    void stop(){/* 내용 생략 */}
+    void rew(){/* 내용 생략 */}
+    void ff(){/* 내용 생략 */}
+}
+
+public class TVCR extends Tv{
+    VCR vcr = new VCR();
+
+    void play(){
+        vcr.play();
+    }
+}
